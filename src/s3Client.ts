7aa4b1c69fd6b0b8b9e93d51e71b192689cd82f3
furyGo <99458191+furyGo@users.noreply.github.com>
2022-06-09 15:33:15 +0800

    Update s3Client.ts

--- src/s3Client.ts
+++ src/s3Client.ts
@@ -3,7 +3,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 const REGION_US_EAST_1 = "us-east-1";
 const s3Client = new S3Client({
     region: REGION_US_EAST_1,
-    endpoint: "https://endpoint.foreverland.org",
+    endpoint: "https://endpoint.foreverland.co",
     credentials: {
         accessKeyId: "YourAccessKeyId",
         secretAccessKey: "YourSecretAccessKey",
